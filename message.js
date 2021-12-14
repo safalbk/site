6add81046036bfa0c3c0a7d023d78cf86838c850
safalbk <safalbk333@gmail.com>
2021-12-14 19:30:40 +0530

    Add files via upload

---
+++ message.js
@@ -0,0 +1,61 @@
+var on=0;
+function off() {
+		
+            var refresh = 1000; // Refresh rate in milli seconds
+            mytime = setTimeout('bulb()', refresh);
+			
+
+        }
+function bulb(){ 
+
+ if (on == 0) {
+document.getElementById('bulb1').src='assets/bulb.png' ;
+document.getElementById('bulb2').src='assets/bulb.png' ;
+document.getElementById('bulb3').src='assets/bulb.png' ;
+document.getElementById('bulb4').src='assets/bulb.png' ;
+document.getElementById('bulb5').src='assets/bulb.png' ;
+document.getElementById('bulb6').src='assets/bulb.png' ;
+
+    } 
+	if (on == 1) {
+document.getElementById('bulb1').src='assets/bulb_blue.png' ;
+document.getElementById('bulb2').src='assets/bulb_green.png' ;
+document.getElementById('bulb3').src='assets/bulb_orange.png' ;
+document.getElementById('bulb4').src='assets/bulb_pink.png' ;
+document.getElementById('bulb5').src='assets/bulb_red.png' ;
+document.getElementById('bulb6').src='assets/bulb_yellow.png' ;
+    } 
+	
+	if (on == 0)
+	{
+		on=1;
+	}
+	else{
+		on=0;
+	}
+	
+	
+	off();
+}  
+function bulboff(){  
+document.getElementById('bulb1').src='assets/bulb.png' ;
+bulb1();
+off();
+
+}  
+var id = null;
+function myMove() {
+  var elem = document.getElementById("myAnimation");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    }
+  }
+}
